1c321cab3ab869156229821316f128715745c243
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-08-02 15:30:08 +0200

    Abstract class GenericElement

---
+++ src/main/java/GenericElement.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+
+
+public abstract class GenericElement implements Serializable{
+    protected String codiceISBN;
+    protected String titolo;
+    protected int annoPubblicazione;
+    protected int numeroPagine;
+
+    public GenericElement(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine) {
+        this.codiceISBN = codiceISBN;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getCodiceISBN() {
+        return codiceISBN;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    @Override
+    public String toString() {
+        return "ElementoCatalogo{" +
+                "codiceISBN='" + codiceISBN + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                '}';
+    }
+}
