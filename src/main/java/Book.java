65aa1c8e6fcc008abf72385f402872f7b33408cf
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-08-02 15:55:49 +0200

    Book/Magazine

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,4 +1,31 @@
-public class Book {
+public class Book extends GenericElement {
+    private String author;
+    private String genre;
 
+    public Book(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine, String autore, String genere) {
+        super(codiceISBN, titolo, annoPubblicazione, numeroPagine);
+        this.author = autore;
+        this.genre = genere;
+    }
 
+    public String getAutore() {
+        return author;
+    }
+
+    public String getGenere() {
+        return genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "codiceISBN='" + codiceISBN + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                ", autore='" + author + '\'' +
+                ", genere='" + genre + '\'' +
+                '}';
+    }
 }
+
--- src/main/java/Magazine.java
+++ src/main/java/Magazine.java
@@ -1,3 +1,28 @@
-public class Magazine  {
+public class Magazine extends GenericElement {
+    public enum Periodicita {
+        SETTIMANALE, MENSILE, SEMESTRALE
+    }
 
+    private Periodicita periodicita;
+
+    public Magazine(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(codiceISBN, titolo, annoPubblicazione, numeroPagine);
+        this.periodicita = periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    @Override
+    public String toString() {
+        return "Magazine{" +
+                "codiceISBN='" + codiceISBN + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                ", periodicita=" + periodicita +
+                '}';
+    }
 }
+
